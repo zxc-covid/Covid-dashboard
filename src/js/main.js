344ffea827c533703e431b8c64b797e1451a823d
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2020-12-19 13:06:49 +0300

    feat: implement global-cases feature

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,11 @@
-// all imports for js will be here
+// all imports for js
+import './global-cases/global-cases';
+
+// main imports!
+import '../styles/reset.sass';
+import '../assets/fonts/fonts.sass';
 import '../styles/styles.sass';
+import '../styles/loading-wheel.sass';
+
+// all imports for styles
+import '../styles/global-cases/global-cases.sass';
