2f0837e90e865c13f6d3c7956b4e9f7f8dd2831b
ShaxxKh <sop.ugc2@gmail.com>
2020-12-22 02:08:41 +0500

    feat: make a skeleton of app

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
 // all imports for js will be here
 import '../styles/styles.sass';
+import '../styles/header/header.sass';
+import '../styles/region-stats/cases.sass';
+import '../styles/map/map.sass';
+import '../styles/death-stats/death.sass';
+import '../styles/footer/footer.sass';
