afbbd6ac63d568bf6bb3afc73e45ae35c4a40a69
ShaxxKh <sop.ugc2@gmail.com>
2020-12-23 01:22:36 +0500

    feat: implement death-recovered feature and add death-recovered visual

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,18 @@
 // all imports for js
 import './global-cases/global-cases';
+import './death-stats/death-stats';
+import './region-stats/region-stats';
+import './death-stats/recovered-stats';
 
 // main imports!
 import '../styles/reset.sass';
 import '../assets/fonts/fonts.sass';
 import '../styles/styles.sass';
 
-import './region-stats/region-stats';
 import '../styles/header/header.sass';
 import '../styles/region-stats/cases.sass';
 import '../styles/map/map.sass';
-import '../styles/death-stats/death.sass';
+import '../styles/death-stats/death-recovered.sass';
 import '../styles/footer/footer.sass';
 
 import '../styles/loading-wheel.sass';
