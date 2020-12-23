02bc73ef8a633b33cd909c3bbf1aaf33abe08abd
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2020-12-23 23:08:26 +0300

    fix: styles

--- src/js/region-stats/region-stats.js
+++ src/js/region-stats/region-stats.js
@@ -14,9 +14,12 @@ function appendTotalCasesByCountries(countries) {
   sortByTotalConfirmed(countries);
   for (let i = 0; i < countries.length; i += 1) {
     const countryContainer = document.createElement('div');
+    countryContainer.classList.add('cases-container');
     const confirmedCases = document.createElement('span');
+    confirmedCases.classList.add('cases-amount');
     confirmedCases.textContent = countries[i].TotalConfirmed;
     const countryName = document.createElement('span');
+    countryName.classList.add('cases-country');
     countryName.textContent = countries[i].Country;
     countryContainer.append(confirmedCases, countryName);
     regionStatsContainer.appendChild(countryContainer);
