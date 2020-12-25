404e8045f29e288d453454b268462fa238f68564
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2020-12-25 18:10:43 +0300

    fix: footer link

--- src/js/footer/footer.js
+++ src/js/footer/footer.js
@@ -1,6 +1,6 @@
-const Developers = document.querySelector('.developers-wrapper');
+const Footer = document.querySelector('.footer');
 
-Developers.addEventListener('click', (e) => {
+Footer.addEventListener('click', (e) => {
   if (e.target.className === 'developer') {
     document.location.href = e.target.dataset.link;
   }
