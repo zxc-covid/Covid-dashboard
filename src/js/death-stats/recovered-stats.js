47bd81008702af0f103d0c62d25b4ef4fc48c148
ShaxxKh <sop.ugc2@gmail.com>
2020-12-27 23:17:11 +0500

    fix: fix api of death and recovered

--- src/js/death-stats/recovered-stats.js
+++ src/js/death-stats/recovered-stats.js
@@ -6,52 +6,18 @@ const requestOption = {
 };
 
 const recoveredMode = {
-  last: { method: appendRecoveredPerHundredThousandPeople, api: 'https://disease.sh/v3/covid-19/countries?sort=recovered' },
-  current: { method: appendGlobalRecovered, api: 'https://api.covid19api.com/summary' },
-  next: { method: appendLastDayRecovered, api: 'https://api.covid19api.com/summary' },
+  last: { method: appendRecoveredPerHundredThousandPeople, api: 'https://disease.sh/v3/covid-19/countries?sort=recovered', type: '100k' },
+  current: { method: appendRecoveredByCountries, api: 'https://disease.sh/v3/covid-19/countries?sort=recovered', type: 'global' },
+  next: { method: appendLastDayRecovered, api: 'https://disease.sh/v3/covid-19/countries?yesterday=true&sort=todayRecovered', type: 'last-day' },
 };
 
-function moveForward() {
-  const mode = recoveredMode.current;
-  recoveredMode.current = recoveredMode.next;
-  recoveredMode.next = recoveredMode.last;
-  recoveredMode.last = mode;
-  document.querySelector('.recovered__subheading').remove();
-  document.getElementById('recovered-controls').remove();
-  document.querySelector('.recovered__by-countries-container').remove();
-  getData(recoveredMode.current.method, recoveredMode.current.api);
+async function getData(func, api) {
+  await fetch(api, requestOption)
+    .then((response) => response.json())
+    .then((json) => func(json));
 }
 
-function moveBack() {
-  const mode = recoveredMode.current;
-  recoveredMode.current = recoveredMode.last;
-  recoveredMode.last = recoveredMode.next;
-  recoveredMode.next = mode;
-  document.querySelector('.recovered__subheading').remove();
-  document.getElementById('recovered-controls').remove();
-  document.querySelector('.recovered__by-countries-container').remove();
-  getData(recoveredMode.current.method, recoveredMode.current.api);
-}
-
-function sortCountries(countries, method) {
-  if (method === 'TotalRecovered') {
-    countries.sort((a, b) => (a.TotalRecovered < b.TotalRecovered ? 1 : -1));
-  } else if (method === 'NewRecovered') {
-    countries.sort((a, b) => (a.NewRecovered < b.NewRecovered ? 1 : -1));
-  }
-  return countries;
-}
-
-function appendGlobalRecovered(data) {
-  const global = data.Global;
-  const countries = data.Countries;
-  const recoveredByCountriesContainer = document.createElement('div');
-  recoveredByCountriesContainer.classList.add('recovered__by-countries-container');
-
-  const recoveredSubheading = document.createElement('h3');
-  recoveredSubheading.classList.add('recovered__subheading');
-  recoveredSubheading.textContent = global.TotalRecovered;
-
+function addControls(type) {
   const recoveredControls = document.createElement('div');
   recoveredControls.classList.add('graph-controls');
   recoveredControls.id = 'recovered-controls';
@@ -66,17 +32,59 @@ function appendGlobalRecovered(data) {
 
   const recoveredDescription = document.createElement('p');
   recoveredDescription.classList.add('graph--description');
-  recoveredDescription.classList.add('recovered--description');
-  recoveredDescription.innerText = 'Total Recovered';
 
-  sortCountries(countries, 'TotalRecovered');
+  if (type === 'global') {
+    recoveredDescription.innerText = 'Total Recovered';
+  } else if (type === 'last-day') {
+    recoveredDescription.innerText = 'Last Day';
+  } else if (type === '100k') {
+    recoveredDescription.innerText = 'Per 100k people';
+  }
+  recoveredControls.append(LeftArrow, recoveredDescription, RightArrow);
+  recoveredContainer.append(recoveredControls);
+}
+
+async function moveForward() {
+  const mode = recoveredMode.current;
+  recoveredMode.current = recoveredMode.next;
+  recoveredMode.next = recoveredMode.last;
+  recoveredMode.last = mode;
+  document.querySelector('.recovered__by-countries-container').remove();
+  document.getElementById('recovered-controls').remove();
+  await getData(recoveredMode.current.method, recoveredMode.current.api);
+  addControls(recoveredMode.current.type);
+}
+
+async function moveBack() {
+  const mode = recoveredMode.current;
+  recoveredMode.current = recoveredMode.last;
+  recoveredMode.last = recoveredMode.next;
+  recoveredMode.next = mode;
+  document.querySelector('.recovered__by-countries-container').remove();
+  document.getElementById('recovered-controls').remove();
+  await getData(recoveredMode.current.method, recoveredMode.current.api);
+  addControls(recoveredMode.current.type);
+}
+
+function appendGlobalRecovered(global) {
+  const recoveredSubheading = document.createElement('h3');
+  recoveredSubheading.classList.add('recovered__subheading');
+  recoveredSubheading.textContent = global.recovered;
+
+  recoveredContainer.appendChild(recoveredSubheading);
+}
+
+function appendRecoveredByCountries(countries) {
+  const recoveredByCountriesContainer = document.createElement('div');
+  recoveredByCountriesContainer.classList.add('recovered__by-countries-container');
+
   for (let i = 0; i < countries.length; i += 1) {
     const countryContainer = document.createElement('div');
     countryContainer.classList.add('recovered__country-container');
 
     const recoveredByCountries = document.createElement('h4');
     recoveredByCountries.classList.add('recovered__count-by-countries');
-    recoveredByCountries.textContent = countries[i].TotalRecovered;
+    recoveredByCountries.textContent = countries[i].recovered;
 
     const descriptorOfNumber = document.createElement('span');
     descriptorOfNumber.classList.add('recovered__number-description');
@@ -84,50 +92,26 @@ function appendGlobalRecovered(data) {
 
     const countryName = document.createElement('h4');
     countryName.classList.add('recovered__country-name');
-    countryName.textContent = countries[i].Country;
+    countryName.textContent = countries[i].country;
 
     recoveredByCountries.appendChild(descriptorOfNumber);
     countryContainer.append(recoveredByCountries, countryName);
     recoveredByCountriesContainer.append(countryContainer);
   }
-  recoveredControls.append(LeftArrow, recoveredDescription, RightArrow);
-  recoveredContainer.append(recoveredSubheading, recoveredByCountriesContainer, recoveredControls);
+  recoveredContainer.appendChild(recoveredByCountriesContainer);
 }
 
-function appendLastDayRecovered(data) {
-  const global = data.Global;
-  const countries = data.Countries;
+function appendLastDayRecovered(countries) {
   const recoveredByCountriesContainer = document.createElement('div');
   recoveredByCountriesContainer.classList.add('recovered__by-countries-container');
 
-  const recoveredSubheading = document.createElement('h3');
-  recoveredSubheading.classList.add('recovered__subheading');
-  recoveredSubheading.textContent = global.NewRecovered;
-
-  const recoveredControls = document.createElement('div');
-  recoveredControls.classList.add('graph-controls');
-  recoveredControls.id = 'recovered-controls';
-
-  const RightArrow = document.createElement('button');
-  RightArrow.classList.add('graph-arrow--right');
-  RightArrow.addEventListener('click', moveForward);
-
-  const LeftArrow = document.createElement('button');
-  LeftArrow.classList.add('graph-arrow--left');
-  LeftArrow.addEventListener('click', moveBack);
-
-  const recoveredDescription = document.createElement('p');
-  recoveredDescription.classList.add('graph--description');
-  recoveredDescription.innerText = 'Last Day';
-
-  sortCountries(countries, 'NewRecovered');
   for (let i = 0; i < countries.length; i += 1) {
     const countryContainer = document.createElement('div');
     countryContainer.classList.add('recovered__country-container');
 
     const recoveredByCountries = document.createElement('h4');
     recoveredByCountries.classList.add('recovered__count-by-countries');
-    recoveredByCountries.textContent = countries[i].NewRecovered;
+    recoveredByCountries.textContent = countries[i].todayRecovered;
 
     const descriptorOfNumber = document.createElement('span');
     descriptorOfNumber.classList.add('recovered__number-description');
@@ -135,15 +119,14 @@ function appendLastDayRecovered(data) {
 
     const countryName = document.createElement('h4');
     countryName.classList.add('recovered__country-name');
-    countryName.textContent = countries[i].Country;
+    countryName.textContent = countries[i].country;
 
     recoveredByCountries.appendChild(descriptorOfNumber);
     countryContainer.append(recoveredByCountries, countryName);
     recoveredByCountriesContainer.append(countryContainer);
   }
   /* document.querySelector('.lds--global-cases').remove(); */
-  recoveredControls.append(LeftArrow, recoveredDescription, RightArrow);
-  recoveredContainer.append(recoveredSubheading, recoveredByCountriesContainer, recoveredControls);
+  recoveredContainer.append(recoveredByCountriesContainer);
 }
 
 function appendRecoveredPerHundredThousandPeople(data) {
@@ -159,30 +142,6 @@ function appendRecoveredPerHundredThousandPeople(data) {
   const recoveredByCountriesContainer = document.createElement('div');
   recoveredByCountriesContainer.classList.add('recovered__by-countries-container');
 
-  const recoveredSubheading = document.createElement('h3');
-  recoveredSubheading.classList.add('recovered__subheading');
-
-  fetch('https://disease.sh/v3/covid-19/all', requestOption)
-    .then((response) => response.json())
-    .then((json) => {
-      document.querySelector('.recovered__subheading').textContent = (json.recovered / (json.population / 100000)).toFixed(2);
-    });
-
-  const recoveredControls = document.createElement('div');
-  recoveredControls.classList.add('graph-controls');
-  recoveredControls.id = 'recovered-controls';
-
-  const RightArrow = document.createElement('button');
-  RightArrow.classList.add('graph-arrow--right');
-  RightArrow.addEventListener('click', moveForward);
-
-  const LeftArrow = document.createElement('button');
-  LeftArrow.classList.add('graph-arrow--left');
-  LeftArrow.addEventListener('click', moveBack);
-
-  const recoveredDescription = document.createElement('p');
-  recoveredDescription.classList.add('graph--description');
-  recoveredDescription.innerText = 'Per 100k People';
   for (let i = 0; i < customizedData.length; i += 1) {
     const countryContainer = document.createElement('div');
     countryContainer.classList.add('recovered__country-container');
@@ -203,13 +162,13 @@ function appendRecoveredPerHundredThousandPeople(data) {
     countryContainer.append(recoveredByCountries, countryName);
     recoveredByCountriesContainer.append(countryContainer);
   }
-  recoveredControls.append(LeftArrow, recoveredDescription, RightArrow);
-  recoveredContainer.append(recoveredSubheading, recoveredByCountriesContainer, recoveredControls);
+  recoveredContainer.appendChild(recoveredByCountriesContainer);
 }
 
-function getData(func, api) {
-  fetch(api, requestOption)
-    .then((response) => response.json())
-    .then((json) => func(json));
+async function init() {
+  await getData(appendGlobalRecovered, 'https://disease.sh/v3/covid-19/all');
+  await getData(appendRecoveredByCountries, 'https://disease.sh/v3/covid-19/countries?sort=recovered');
+  addControls('global');
 }
-getData(appendGlobalRecovered, 'https://api.covid19api.com/summary');
+
+init();
